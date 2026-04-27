d7d55d7551ad4beaa2345d5799ac05ed65da30b4
Yogendra Sodha <yuvisodha@gmail.com>
2026-04-26 22:55:43 -0400

    chore: clean up leftover debug files and update gitignore

--- demo.py
+++
@@ -1,7 +0,0 @@
-from titan_db import TitanClient
-import time
-
-db = TitanClient(["http://127.0.0.1:8001", "http://127.0.0.1:8002", "http://127.0.0.1:8003"])
-print(db.status())
-db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)")
-print("Success")
